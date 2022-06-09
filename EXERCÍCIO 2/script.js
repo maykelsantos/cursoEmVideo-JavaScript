5dfb8cfbfa2c9b000a873728feffcf8d1a864392
Maykel Santos <81576508+maykelsantos@users.noreply.github.com>
2022-06-08 22:18:03 -0300

    update

--- EXERCÍCIO 2/script.js
+++ EXERCÍCIO 2/script.js
@@ -1,18 +1,11 @@
-function carregar() {
-    var msg = document.getElementById('msg');
-    var img = document.getElementById('foto');
-    var data = new Date();
-    var hora = data.getHours();
-    var minutos = data.getMinutes();
-    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
-    if (hora >= 0 && hora <12) {
-        img.src = './imagens/manha.png'
-        document.body.style.background = `#BF9004`
-    } else if (hora >= 12 && hora <= 18) {
-        img.src = './imagens/tarde.png'
-        document.body.style.background = `#734002`
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var formularioAno = document.getElementById(`txtano`)
+    var res = document.querySelector(`div#res`)
+    if (formularioAno.value.length == 0 || formularioAno.value > ano) {
+        alert(`[ERROR] Verifique os dados e tente novamente!`)
     } else {
-        img.src = './imagens/noite.png'
-        document.body.style.background = '#193540'
+        alert(`TUDO OK!`)
     }
 }
